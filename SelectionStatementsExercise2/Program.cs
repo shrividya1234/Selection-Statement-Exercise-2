31a9ff5722c292cef54dda91918cf4615139f76f
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-15 06:30:12 -0400

    Excersise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Collections;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("What is your favorite subject?");
+            string favoriteSubject = Console.ReadLine();
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is my favorite subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is my favorite subject!");
+                    break;
+                case "French":
+                    Console.WriteLine("French is my favorite subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is my favorite subject!");
+                    break;
+                case "History":
+                    Console.WriteLine("History is my favorite subject!");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand what is my favorite subject!");
+                    break;
+            }
+    }
     }
 }
